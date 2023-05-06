cf2513e96b40aaf2346189151b859f5fc54f0474
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-07 00:17:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { Box } from "@chakra-ui/react";
-import Footer from "./footer/Footer";
-import NavBar from "./header/NavBar";
-import Hero from "./home/Hero";
 import PopularFlights from "./home/PopularFlights";
-import TrendingActivities from "./home/TrendingActivities";
-import WhatDoPeopleFeel from "./home/WhatDoPeopleFeel";
-import BestFlight from "./home/BestFlight";
-
- 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Hero />
-     
       <PopularFlights />
-      <BestFlight/>
-     <TrendingActivities/>
-      <WhatDoPeopleFeel/>
-      <Footer/>
     </div>
   );
 }
--- src/home/PopularFlights.jsx
+++ src/home/PopularFlights.jsx
@@ -4,33 +4,15 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './popular.css';
 import l1 from "../img/i1.jpg"
-import l2 from "../img/i2.jpg"
-import l3 from "../img/i3.jpg"
-import l4 from "../img/i4.jpg"
-import { Heading, Image, Text } from '@chakra-ui/react';
-import next from "../img/next.png"
-import prev from "../img/prev.png"
+import { Image } from '@chakra-ui/react';
 function App() {
-  
-
-  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
-    <img src={prev} alt="nextArrow"  position={'absolute'} top={'30%'} {...props}  width={'300px'}/>
-    
-  );
-
-  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
-    <img src={next} alt="nextArrow"  position={'absolute'} top={'30%'} {...props}  width={'300px'}/>
-  );
-
    const settings = {
-    dots: true,
-    infinite: false,
+    dots: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
      initialSlide: 0,
-     prevArrow: <SlickArrowLeft />,
-     nextArrow: <SlickArrowRight />,
     responsive: [
       {
         breakpoint: 1024,
@@ -38,7 +20,7 @@ function App() {
           slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
-          dots: true,
+          dots: false,
         },
       },
       {
@@ -59,50 +41,38 @@ function App() {
     ],
   };
 
- 
-
   return (
     <div className="App">
-      <Heading>Popular Flights Near You</Heading>
-      <Text>Find dashing deals on domestic and international flights!</Text>
     <Slider {...settings}>
-       
         <div className="card">
-          <div className="card-top">
             <Image
               src={
                 l1
               } 
             />
-          </div> 
         </div>
         <div className="card">
-          <div className="card-top">
             <Image
               src={
-                l2
+                l1
               } 
             />
-          </div> 
         </div>
         <div className="card">
-          <div className="card-top">
             <Image
               src={
-                l3
+                l1
               } 
             />
-          </div> 
         </div>
         <div className="card">
-          <div className="card-top">
             <Image
               src={
-                l4
+                l1
               } 
             />
-          </div> 
         </div>
+         
       
     </Slider>
   </div>
